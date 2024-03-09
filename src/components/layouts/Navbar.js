9067cb82beca059527b9de1f0fe764353bbf4a80
Larissaggarcia <larissaggarcia@gmail.com>
2024-03-09 07:36:27 -0300

    Home files and navbar

---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,13 @@
+import styles from './Navbar.module.css'
+import logo from '../../images/logo-vtex.svg'
+
+function Navbar (){
+
+    return(
+        <nav className={styles.navbar}>
+            <img src={logo} alt="vtex" />
+        </nav>
+    )
+}
+
+export default Navbar
